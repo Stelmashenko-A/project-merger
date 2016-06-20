7861b6b4d0d8e32b0edcab7d50e595030d858366
Stelmashenko-A <stelma_28@bk.ru>
2016-06-20 21:43:00 +0300

    Environment setup

---
+++ ProjectMerger/ProjectMerger.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectMerger.Web.Api.WebApiApplication" Language="C#" %>
---
+++ ProjectMerger/ProjectMerger.Web.Client/index.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./style.css');
+var Header = require('./modules/header/header');
+ReactDOM.render(<Header/>, document.getElementById('root'));
+
